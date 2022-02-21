896e53ba88687a5f97d426b0ce7e8f7838130f66
Javi <javier.t.sartore@gmail.com>
2022-02-21 16:29:04 -0300

    getters y mapGetters

--- src/store.js
+++ src/store.js
@@ -5,12 +5,24 @@ const store = createStore({
   state () {
     return {
       count: 1,
-      usuario: {codigo:1, nombre:'pepe'}
+      usuario: {codigo:1, nombre:'pepe'},
+      productos: [{codigo: 1, descripcion: 'Tele'}, 
+                  {codigo: 2, descripcion: 'Cocina'},
+                  {codigo: 3, descripcion: 'Microondas'},
+                ]
     }
   },
   mutations: {
     increment (state) {
       state.count++
+    }
+  },
+  getters: {                   //Todos los métodos getters llevan como parametro el state
+    getcantidadProductos(state){
+      return state.productos.length
+    },
+    getProductoPorCodigo:(state) => (codigo) => {
+      return state.productos.find(producto => producto.codigo == codigo)
     }
   }
 })
--- src/views/Sistema.vue
+++ src/views/Sistema.vue
@@ -4,27 +4,40 @@
       <p>Estado {{this.$store.state.count}}</p>
       <p>Estado desde ... mapState {{count}}</p> 
       <p>Usuario desde ... mapState {{usuario}}</p> 
+      <p>Cantidad de productos {{cantidadProductos}}</p>
+      <p>Cantidad de productos desde getters {{this.$store.getters.getcantidadProductos}}</p>
+      <p>Producto código 2 por getters {{this.$store.getters.getProductoPorCodigo(2)}}</p>
+      <br>
+      <br>
+      <p>Cantidad de productos desde mapGetters {{getcantidadProductos}}</p>
+      <p>Producto código 2 por mapGetters {{getProductoPorCodigo(3)}} </p>
 
   </div>
 </template>
 
 <script>
 
-import{mapState} from 'vuex'
+import{mapState, mapGetters} from 'vuex'
 
 export default {
 name: 'SistemaView',
-/* computed: {                                                      
-    count(){
-        return this.$store.state.count
-    }
-}, */
+    /* computed: {                                                      
+            count(){
+            return this.$store.state.count
+        }
+    }, */
 
-//Este count que hacemos referencia en la linea 22 seria el count del storage o store
-//Los ... --> significa el operador spread
-computed: {
-    ...mapState(['count', 'usuario'])
-}
+    //Este count que hacemos referencia en la linea 22 seria el count del storage o store
+    //Los ... --> significa el operador spread
+    computed: {
+        ...mapState(['count', 'usuario']),
+
+            //Para acceder a la lista de productos del store
+            cantidadProductos (){
+                    return this.$store.state.productos.length
+           },
+           ...mapGetters(['getcantidadProductos', 'getProductoPorCodigo'])
+    }
 }
 </script>
 
